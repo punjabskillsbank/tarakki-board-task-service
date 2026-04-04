dee2cba3e543f746395ff9e55a4132727fcd6f63
Ivkaran Singh <ivkaransingh@macbook-pro.mynetworksettings.com>
2026-04-04 16:42:29 -0400

    first commit

---
+++ src/main/java/com/tarakki/boardtask/TarakkiBoardTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tarakki.boardtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarakkiBoardTaskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarakkiBoardTaskServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarakki/boardtask/TarakkiBoardTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarakki.boardtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarakkiBoardTaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

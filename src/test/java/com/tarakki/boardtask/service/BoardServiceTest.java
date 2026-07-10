d1f788e1a464de96d586d1518e7991123ea2a6fe
Ivkaran Singh <ivkaransingh@macbook-pro.mynetworksettings.com>
2026-07-10 01:17:05 -0400

    TK_84: revert unit test

--- src/test/java/com/tarakki/boardtask/service/BoardServiceTest.java
+++ src/test/java/com/tarakki/boardtask/service/BoardServiceTest.java
@@ -66,7 +66,7 @@ class BoardServiceTest {
         BoardDTO result = boardService.createBoard(dto);
 
         assertNotNull(result);
-        assertNotEquals(dto.getBoardName(), result.getBoardName());
+        assertEquals(dto.getBoardName(), result.getBoardName());
         assertEquals(dto.getBoardDesc(), result.getBoardDesc());
         assertEquals(dto.getOrgId(), result.getOrgId());
         assertEquals(dto.getCreatedBy(), result.getCreatedBy());

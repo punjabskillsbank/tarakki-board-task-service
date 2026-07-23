1d332bf80a73a7142f028650986119a10ffbb7ad
codebyjsa <jsaishere@gmail.com>
2026-07-23 10:45:07 +0530

    TK_98: update dev and prod profile properties to use environment variables

--- src/test/java/com/tarakki/boardtask/util/BoardTestDataFactory.java
+++ src/test/java/com/tarakki/boardtask/util/BoardTestDataFactory.java
@@ -22,12 +22,14 @@ public class BoardTestDataFactory {
     public static final Long INVALID_ORG_ID = 999L;
     public static final Long VALID_BOARD_ID = 1L;
 
+    public static final UUID CREATED_BY = UUID.fromString("2af05d6e-b8db-4fe2-8385-d2a737332521");
+
     public static BoardDTO createBoardDTO() {
         BoardDTO dto = new BoardDTO();
         dto.setOrgId(VALID_ORG_ID);
         dto.setBoardName("Project Board");
         dto.setBoardDesc("Board for project tasks");
-        dto.setCreatedBy(UUID.randomUUID());
+        dto.setCreatedBy(CREATED_BY);
         return dto;
     }
 
@@ -44,7 +46,7 @@ public class BoardTestDataFactory {
         board.setOrgId(VALID_ORG_ID);
         board.setBoardName("Project Board");
         board.setBoardDesc("Board for project tasks");
-        board.setCreatedBy(UUID.randomUUID());
+        board.setCreatedBy(CREATED_BY);
         return board;
     }
 

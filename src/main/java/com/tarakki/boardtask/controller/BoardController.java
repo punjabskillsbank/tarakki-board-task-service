940cd7f9ce39bfb3aa2145c63b6a2ef1afd9493f
ishmeet-codes <ishmeetsinghxa@gmail.com>
2026-09-02 21:49:49 +0530

    TK_87: Removed the shared logic of kafka and Aop

--- src/main/java/com/tarakki/boardtask/controller/BoardController.java
+++ src/main/java/com/tarakki/boardtask/controller/BoardController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.tarakki.boardtask.annotation.Auditable;
+import com.tarakki.common.audit.annotation.Auditable;
 import com.tarakki.boardtask.entity.Board;
 
 import java.util.List;
